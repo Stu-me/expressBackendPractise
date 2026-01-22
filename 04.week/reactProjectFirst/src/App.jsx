fe59d5e77a92ebf24c79116fa0a294a111c29d34
Stu-me <stusundram@gmail.com>
2026-01-22 11:03:22 +0530

    happy cause made dynamic todo basic structure

--- 04.week/reactProjectFirst/src/App.jsx
+++ 04.week/reactProjectFirst/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- const [todos, setTodos] = useState(
+ const [todos, setTodos] = useState( // this  was like declaring todos hard coded 
   [{
     title:"go to gym ",
     description:"Go to gym 2hrs daily",
@@ -12,29 +12,49 @@ function App() {
     title: "Study Dsa",
     description:"Study daily atleast 1 hour",
     completed:true
+  },
+{
+    title: "new task",
+    description:"lesser the turnaround time ",
+    completed:true
   }]
  );
 
+ function addTodo() { // this task expand the todo and add my task at the end
+  setTodos([...todos,{
+    title:"newly added one",
+    description:"lets see how thing work here"
+  }])
+ }
+
   return (
     <div>
-      {JSON.stringify(todos)}
-      <Todo title="lets see how things work here " description="it worked "></Todo>
-
-      {/* customButton technique is xml where we declare out own syntax and use it  */}
+      {/* <Todo title={todos[0].title} description={todos[0].description}></Todo>
+      <Todo title={todos[1].title} description={todos[1].description}></Todo> */
+      
+      // better and safe way is to use automation cause we wont be writing 1000 lines of todos for the 1000 todos
+      // so we can use map or any other means 
+      }
+      {<button onClick={addTodo}>Adds a hardcoded task to it   </button>}
+      {
+        todos.map(function(todo){
+          return <Todo title={todo.title} description={todo.description}></Todo>
+        })
+      }
     </div>
   )
 }
 
 //function for todo
-function Todo(props){
+function Todo(props){ // this one we declare our own tag and then we use it 
   return <div>
     <h1>{props.title}</h1>
-    <h1>{props.description}</h1>
+    <h3>{props.description}</h3>
   </div>
 }
 
 // component
-function CustomButton(props){  
+function CustomButton(props){   // this is customized xml tag 
 
   function onClickHandler(){
     props.setCount(props.count+1);
